949117965d6d7e63232f025f66ea43e45595ffcc
Sergey Ivannikov <bearzerk@gmail.com>
2020-05-21 19:24:18 +0300

    new tasks added, i hate multi dimensional arrays, and iterations over arrays

--- Problems/Check sudoku/src/Main.java
+++ Problems/Check sudoku/src/Main.java
@@ -14,18 +14,47 @@ public class Main {
             }
         }
 
+//        for (int i = 0; i < field.length; i++) {
+//            int[] array = new int[n * n];
+//            array = Arrays.copyOf(field[i], n * n);
+//            Arrays.sort(array);
+//            if (n == 1 && array[0] != 1) {
+//                isSolved = false;
+//                break;
+//            }
+//            for (int j = 1; j < array.length; j++) {
+//                if (array[j - 1] != 0 && array[j - 1] + 1 != array[j]) {
+//                    isSolved = false;
+//                    break;
+//                }
+//            }
+//        }
+//        for (int i = 0; i < field.length; i++) {
+//            int[] array = new int[field.length];
+//            for (int j = 0; j < field[i].length; j++) {
+//                array[j] = field[j][i];
+//            }
+//            Arrays.sort(array);
+//            for (int j = 1; j < array.length; j++) {
+//                if (array[j - 1] != 0 && array[j - 1] + 1 != array[j]) {
+//                    isSolved = false;
+//                    break;
+//                }
+//            }
+//        }
+
         for (int i = 0; i < field.length; i++) {
-            int[] array = new int[n * n];
-            array = Arrays.copyOf(field[i], n * n);
-            Arrays.sort(array);
-            if (n == 1 && array[0] != 1) {
+            int[] a = Arrays.copyOf(field[i], field.length);
+            if (field.length == 1 && a[0] != 1) {
                 isSolved = false;
                 break;
             }
-            for (int j = 1; j < array.length; j++) {
-                if (array[j - 1] != 0 && array[j - 1] + 1 != array[j]) {
-                    isSolved = false;
-                    break;
+            for (int j = i + 1; j < field.length; j++) {
+                for (int k = 0; k < field.length; k++) {
+                    if (a[k] == field[j][k] || a[k] == 0) {
+                        isSolved = false;
+                        break;
+                    }
                 }
             }
         }
@@ -34,14 +63,16 @@ public class Main {
             for (int j = 0; j < field[i].length; j++) {
                 array[j] = field[j][i];
             }
-            Arrays.sort(array);
-            for (int j = 1; j < array.length; j++) {
-                if (array[j - 1] != 0 && array[j - 1] + 1 != array[j]) {
-                    isSolved = false;
-                    break;
+            for (int j = i + 1; j < field.length; j++) {
+                for (int k = 0; k < field.length; k++) {
+                    if (array[k] == field[k][j]) {
+                        isSolved = false;
+                        break;
+                    }
                 }
             }
         }
+
         System.out.println(isSolved ? "YES" : "NO");
     }
 }
